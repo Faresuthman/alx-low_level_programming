b157f1e2d666132294cb91360eadce02c5af2555
faresuthman <ot05mane@gmail.com>
2023-08-22 15:17:46 -0700

    M3

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an i
+ *
+ * @s: string input parameter
+ *
+ * return: convert i from s
+ */
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	}
+
+	return (num * sign);
+}
