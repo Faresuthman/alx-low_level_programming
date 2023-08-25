135d0cc327fecb4fd4051a250bd70f64e92b26a8
faresuthman <ot05mane@gmail.com>
2023-08-25 11:56:16 -0700

    M3

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,7 +11,7 @@ char *rot13(char *s)
 	int i;
 
 	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char ROT13[] = "nbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZA";
+	char ROT13[] = "nopqrsruvwxyzabcdefghiijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	char *ptr = s;
 
 	while (*s)
