06f2adf00ee195c0c8d4d279f9dd0e3c00482628
faresuthman <ot05mane@gmail.com>
2023-08-25 12:10:20 -0700

    M3

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers chars
+ * @u: integer parans
+ * Return: 0
+ */
+void print_number(int u)
+{
+	usigned int u1;
+
+	u1 = u;
+
+	if (u < 0)
+	{
+		_putchar('-');
+		u1 = -u;
+	}
+
+	if (u1 / 10 != 0)
+	{
+		print_number(u1 / 10);
+	}
+	-putchar((u1 % 10) + '0');
+}
